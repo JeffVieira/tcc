3b03ae4de04b096f1e03d5dad9b6d35791fb736e
JeffVieira <jeff_malganis@hotmail.com>
2015-03-03 13:27:06 -0300

    breadcrumb beta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def current_enterpreneur
+    id = nil
+    if params[:controller] == 'enterpreneurs' and params[:action] == "show"
+      id = params[:id]
+    elsif params[:enterpreneur_id].present?
+      id = params[:enterpreneur_id]
+    end
+
+    @current_enterpreneur ||= Enterpreneur.where(account: current_account, id: id).first if id and !current_account.nil?
+  end
+
+  def breadcrumb_for_create
+    add_breadcrumb get_model.model_name.human(count: 2), self.controller_path
+    add_breadcrumb "Novo registro - Revisão"
+  end
+
+  def breadcrumb_for_edit
+    add_breadcrumb get_model.model_name.human(count: 2), self.controller_path
+    add_breadcrumb "Edição de registro - Revisão"
+  end
+
+  def get_model
+    self.controller_name.classify.constantize
+  end
+
 end
