83113650db79c0cb613ae9d1626b3b984a01f8d4
JeffVieira <jeff_malganis@hotmail.com>
2015-05-03 19:52:23 -0300

    adicionado imagem de assinatura digital

--- app/models/document.rb
+++ app/models/document.rb
@@ -58,7 +58,7 @@ class Document < ActiveRecord::Base
 
   attr_accessor :icon, :do_version
   after_initialize :set_icon
-  after_create :create_history
+  after_create :create_history, :sign_pdf
 
   scope :aguardando_validacao, ->(user_id) { where(:status=>3, :user_id=>user_id) }
 
@@ -92,17 +92,13 @@ class Document < ActiveRecord::Base
     DocumentHistory.create(document_id: id, user_id: user_id, action: "Criado")
   end
 
-  def prepare_pdf
-    # load a sample PDF file
-
-    mypdf = Origami::PDF.read(self.arquivo.path)
-
+  def prepare_pdf(mypdf = Origami::PDF.read(arquivo.path))
     # location, contact, reason etc are optional parameters
 
     hash_to_be_signed = mypdf.prepare_for_sign(
-      location: "Your Location",
-      contact: "sample@email.com",
-      reason: "Your Reason for signing here,"
+      location: "TCC-FTEC GED",
+      contact: "get-tcc@gmail.com",
+      reason: "Demonstração assinatura digital"
     )
 
     #An Optional parameter :signature_size can be passed, if the signature has different size (default it is: 1111)
@@ -110,26 +106,29 @@ class Document < ActiveRecord::Base
     #send this hash_to_be_signed to a 3rd party application to sign (output must be in PKC#7 format )
 
     #save the PDF for inserting signature later
-    mypdf.save(self.arquivo_file_name)
+    mypdf.save('assinatura_digital/prepared.pdf')
   end
 
   def sign_pdf
-    prepare_pdf
-    mypdf = Origami::PDF.read(self.arquivo.path)
+    mypdf = Origami::PDF.read("prepared.pdf")
 
     # the pdf should be a prepared pdf using the prepare_for_sign method
+    prepare_pdf(mypdf)
 
     # eg signature in PKCS #7 format
-
     signature_base64 = "MIIEUwYJKoZIhvcNAQcCoIIERDCCBEACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHAaCCArgwggK0MIICHaADAgECAggSmghu6WrtzjANBgkqhkiG9w0BAQUFADBSMR8wHQYDVQQDDBZNb2JpbGVFeHByZXNzU3RhZ2luZ0NBMSIwIAYDVQQKDBlNb2JpbGUgRXhwcmVzcyBTdGFnaW5nIENBMQswCQYDVQQGEwJJTjAeFw0xMjA5MDMxMzIzNDNaFw0xNDA5MDMxMzIzNDNaMGYxEzARBgNVBAMMClZhbGltb1Rlc3QxHTAbBgNVBAsMFDg5MTA2MDEyMDYxMzcxNTk4OTFGMQ8wDQYDVQQKDAZWYWxpbW8xEjAQBgoJkiaJk/IsZAEZFgJOUjELMAkGA1UEBhMCSU4wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAJ64wMdgBbuTHhuD2UZvGazKmTDehVy03/yjwH5ZEb7VoLlOUG4RXBg2M8N0i9lqCiO+GI0aKpGP6Tfi9QtmXH8Fkt6VqeWSAVZXVHiMqZGFNAUcKG2JfAUdPTqBCB72nSdn0W6yqxAe4Vj80aux23hMsPVqieNmh0rTZhA2oITfAgMBAAGjfzB9MB0GA1UdDgQWBBQYIur/PPhTKfNKPm5hGf9uEUUgFzAMBgNVHRMBAf8EAjAAMB8GA1UdIwQYMBaAFKhsCBwF5B7wwqeErPH1/46KWJ5cMA4GA1UdDwEB/wQEAwIF4DAdBgNVHSUEFjAUBggrBgEFBQcDAgYIKwYBBQUHAwQwDQYJKoZIhvcNAQEFBQADgYEAJ8zHO0h99vuK+VntfCzfbfKy/6YiJmKkXiU6pFmVGgvTOTTodNTwAQoRx9csaJwOnawPM8IYU+O/ldjvCfD+wycj+AgEPq2Up9N8AbvIcw6dIjRg4b6JBFVYYl6vdnl4N353hQFjxsuDBl4yjHx/rNz7YuDOx4d5XSJwGOSQwX4xggFjMIIBXwIBATBeMFIxHzAdBgNVBAMMFk1vYmlsZUV4cHJlc3NTdGFnaW5nQ0ExIjAgBgNVBAoMGU1vYmlsZSBFeHByZXNzIFN0YWdpbmcgQ0ExCzAJBgNVBAYTAklOAggSmghu6WrtzjAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTIxMDExMTEzNzI5WjAjBgkqhkiG9w0BCQQxFgQUjHEpwYp6rT/FIVWAbbq9lq3/79IwDQYJKoZIhvcNAQEBBQAEgYAoHgDdmt1TrtZ3k8ODQyKOtHAJgYCg1/kdqocREPhz/1U2w+OUOrwhb7u/lh0F+6jhMevzg0RNG7vkJIS1jTS15Kxlv2teW2/VdGSsdF5wmqY837fB6G3IhSuUEAtSZfG7JEAoxg36TTPgeaUxpZf0ER2jRsud9dD5hm14j+5GLw=="
 
     # insert the signature inside the PDF
 
-    mypdf.prepare_for_sign
     mypdf.insert_sign(signature_base64)
 
     # Save the signed PDF
+    build_id_folder
+    mypdf.save("public/assinatura_digital/#{self.id}/#{self.arquivo_file_name}")
+  end
 
-    mypdf.save(self.arquivo_file_name)
+  def build_id_folder
+    dir_path = Rails.root.join("public/assinatura_digital/#{self.id}/")
+    Dir.mkdir(dir_path) unless File.directory?(dir_path)
   end
 end
